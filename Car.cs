e2d59551600f98c9aa6350231c4512327d2e2b16
Kareem Dana <kdana@wtamu.edu>
2022-07-31 15:37:58 -0500

    Changes to .Net 6 Part 2

--- Car.cs
+++ Car.cs
@@ -5,8 +5,8 @@ namespace ClassDemo
     public class Car
     {
         public int CarId {get; set;}
-        public string Make {get; set;}
-        public string Model {get; set;}
+        public string Make {get; set;} = string.Empty;
+        public string? Model {get; set;}
         public int numDoors {get; set;}
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassDemo;
+
+Car carOne = new Car();
+carOne.Make = "Subaru";
+carOne.Model = "Forester";
+carOne.numDoors = 4;
+Console.WriteLine($"Car 1: {carOne.Make} {carOne.Model} {carOne.numDoors}");
+
+Car carTwo = new Car {Make = "Chevrolet", Model = "Silverado", numDoors = 2};
+Console.WriteLine($"Car 2: {carTwo.Make} {carTwo.Model} {carTwo.numDoors}");
